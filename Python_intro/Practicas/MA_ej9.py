f9dcab42188f3e69b719bc887b547bf11e5e917f
Kerszenblat <lara.ker@hotmail.com>
2022-05-02 15:53:34 -0300

    github

--- Python_intro/Practicas/MA_ej9.py
+++ Python_intro/Practicas/MA_ej9.py
@@ -3,6 +3,17 @@
 #Recordá que la frecuencia es la relación entre número de veces que aparece la palabra en cuestión con
 #respecto a la cantidad total de palabras.
 
-"""def _frecuencia():
-    frecuencia = ()
-  with open()"""
+def _frecuencia():
+    frecuencia = {}
+    with open("ejercicio_1.txt", "r") as texto:
+        palabras = texto.read().split()
+        for palabra in palabras:
+            if palabra in frecuencia:
+                frecuencia[palabra] += 1
+            else:
+                frecuencia[palabra] = 1
+        for palabra in frecuencia.keys():
+            frecuencia[palabra] = round(frecuencia[palabra] / len(palabras), 3)
+    return print(frecuencia)
+ 
+_frecuencia()
